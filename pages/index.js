dc118df663c39f32be8185ea7aa4e758c7d33f92
RenatoFrancisco <renatofrancisco.s@gmail.com>
2021-01-27 19:31:46 -0300

    Terceira Aula

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import styled from 'styled-components';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
 
@@ -7,19 +6,11 @@ import db from '../db.json';
 import Widget from '../src/components/Widget';
 import QuizLogo from '../src/components/QuizLogo';
 import QuizBackground from '../src/components/QuizBackground';
+import QuizContainer from '../src/components/QuizContainer';
 import Footer from '../src/components/Footer';
 import GitHubCorner from '../src/components/GitHubCorner';
-
-export const QuizContainer = styled.div`
-  width: 100%;
-  max-width: 350px;
-  padding-top: 45px;
-  margin: auto 10%;
-  @media screen and (max-width: 500px) {
-    margin: auto;
-    padding: 15px;
-  }
-`;
+import Input from '../src/components/Input';
+import Button from '../src/components/Button';
 
 export default function Home() {
   const router = useRouter();
@@ -38,21 +29,20 @@ export default function Home() {
           </Widget.Header>
           <Widget.Content>
             <p>{db.description}</p>
-            <form onSubmit={function (infos) {
+            <form onSubmit={(infos) => {
               infos.preventDefault();
               router.push(`/quiz?name=${name}`);
             }}
             >
-              <input
-                placeholder="Qual seu nome?"
-                onChange={function (infos) {
-                  setName(infos.target.value);
-                }}
+              <Input
+                name="nomeUsuario"
+                onChange={(infos) => setName(infos.target.value)}
+                placeHolder="Qual seu nome?"
+                value={name}
               />
-              <button type="submit" disabled={name.length === 0}>
-                Jogar
-                {name}
-              </button>
+              <Button type="submit" disabled={name.length === 0}>
+                {`Jogar ${name}`}
+              </Button>
             </form>
           </Widget.Content>
         </Widget>
--- src/components/Widget/index.js
+++ src/components/Widget/index.js
@@ -14,7 +14,6 @@ const Widget = styled.div`
     line-height: 1;
     margin-bottom: 0;
   }
-
   p {
     font-size: 14px;
     font-weight: 400;
@@ -45,6 +44,24 @@ Widget.Content = styled.div`
   ul {
     list-style: none;
     padding: 0;
+  }
+`;
+
+Widget.Topic = styled.a`
+  outline: 0;
+  text-decoration: none;
+  color: ${({ theme }) => theme.colors.contrastText};
+  background-color: ${({ theme }) => `${theme.colors.primary}40`};
+  padding: 10px 15px;
+  margin-bottom: 8px;
+  cursor: pointer;
+  border-radius: ${({ theme }) => theme.borderRadius};
+  transition: .3s;
+  display: block;
+  
+  &:hover,
+  &:focus {
+    opacity: .5;
   }
 `;
 
