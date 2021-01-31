fcab0906135fe96ab08ba559e77c9834417c4a6e
RenatoFrancisco <renatofrancisco.s@gmail.com>
2021-01-31 18:04:41 -0300

    Ajuste titulo da pagina

--- pages/index.js
+++ pages/index.js
@@ -34,7 +34,6 @@ export default function Home() {
     <QuizBackground backgroundImage={db.bg}>
       <Head>
         <title>
-          AluraQuiz -
           {db.title}
         </title>
       </Head>
