750394bd890bd2d27959b61bc413238d986bda20
RenatoFrancisco <renatofrancisco.s@gmail.com>
2021-01-31 17:59:05 -0300

    Adicionando favicon

--- pages/_app.js
+++ pages/_app.js
@@ -37,6 +37,7 @@ export default function App({ Component, pageProps }) {
       <Head>
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet" />
+        <link rel="icon" type="image/jpg" sizes="32x32" href="umbrela.jpg" />
       </Head>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
